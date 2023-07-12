5508e15f73ed8663f7bc7d17e71933c164b685b5
CharlyCoderHouse <carlosdiblasi@gmail.com>
2023-07-12 17:00:36 -0300

    15 commit TERCER ENTREGA PURCHASE

--- src/controllers/carts.controller.js
+++ src/controllers/carts.controller.js
@@ -149,17 +149,16 @@ const postPurchase = async(req, res) => {
     // Primero Valido que exista el carrito 
     try {
         const newCart = [];
+        const noStockCart = [];
         // OBTENGO el carrito QUE HAY EN la BASE
         console.log("3 VALIDO CARRITO");
         const cartPuchase = await getCartByIdService(cartId);
-        //console.log("3B " + JSON.stringify(data, null, '\t'));
-        //cartPuchase.push(data)
         console.log("3C" + JSON.stringify(cartPuchase[0], null, '\t'));
         console.log("4 Empiezo a recorrer carrito");
-        cartPuchase[0].products.forEach(async (product) => {
+        cartPuchase[0].products.forEach( (product) => {
             console.log("5 producto" + product.product._id);
             if (product.product.stock > product.quantity) {
-                const resultStock = await stockProductService(product.product._id, product.quantity*-1)
+                const resultStock = stockProductService(product.product._id, product.quantity*-1)
                 //console.log("7 resultado de baja de stock" + resultStock);
                 const prodData = {
                     price: product.product.price,
@@ -167,23 +166,37 @@ const postPurchase = async(req, res) => {
                 }
                 newCart.push(prodData)
                 console.log("8 nuevo carrito" + JSON.stringify(newCart, null, '\t'));
-                const resultDelete = await deleteProductInCartService(cartPuchase[0]._id,product.product._id);
+                const resultDelete = deleteProductInCartService(cartPuchase[0]._id,product.product._id);
                 //console.log("9 resultado de delete de cart" + resultDelete);
+            }else {
+                const prodData = {
+                    id: product._id
+                };
+                noStockCart.push(prodData);
             }
         });
-        console.log("length" + newCart.length);
+        console.log("length " + newCart.length);
         if (newCart.length > 0){
             console.log("10 llamo a grabar ticket");
             const result = await postPurchaseService(newCart, userMail);
             console.log("11: " + JSON.stringify(result, null, '\t'));
-            res.status(200).send({ status: 'success', payload: `Se genero correctamente la compra con el ID ${result.code}`  })
+            if (noStockCart.length > 0) {
+                res.status(200).send({ status: 'success', payload: `Se genero correctamente la compra con el ID ${result.code}  y no pudieron procesarse por falta de stock ${JSON.stringify(noStockCart, null)}`  })
+            } else {
+                res.status(200).send({ status: 'success', payload: `Se genero correctamente la compra con el ID ${result.code}`  })
+            }    
         } else {
-            res.status(404).send({ status: "NOT FOUND", payload: `No hay productos disponible para la compra!` });
-        }
+            if (noStockCart.length > 0) {
+                res.status(404).send({ status: "NOT FOUND", payload: `No pudieron procesarse por falta de stock ${JSON.stringify(noStockCart, null)}` });
+            } else {
+                res.status(404).send({ status: "NOT FOUND", payload: `No hay productos en el carrito!` });
+            }
+        } 
     } catch (error) {
         const response = { status: "Error", payload: error };
         return res.status(404).json(response);
     };
+    
 };
 
 
